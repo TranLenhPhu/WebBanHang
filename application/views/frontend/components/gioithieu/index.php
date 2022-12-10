45ecfaf953e0616c572ad6200a6430e5c6923426
phú lệnh <you@example.com>
2022-12-10 17:10:35 +0700

    uploat 1 lan

---
+++ application/views/frontend/components/gioithieu/index.php
@@ -0,0 +1,25 @@
+<div class="container">
+    <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 list-menu pull-left">
+            <?php $this->load->view('frontend/modules/category'); ?>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 product-content" id="list-content">
+        <div class="product-wrap" style="">
+            <div class="wrap-2" style="margin-bottom: 20px;">
+
+                <h3>Giới thiệu về <span style="color: #8ac400 ">Xuhao store</span></h3>
+                <p>Cùng khám phá thế giới điều hòa tại XuHao, nơi hội tụ những thương hiệu điều hòa hàng đầu
+                    thế giới, sử dụng công nghệ tiên tiến nhất cho từng sản phẩm, các dòng máy inverter tiếp kiệm điện
+                    tối đa và sẽ mang đến không gian cho ngôi nhà bạn sự thoải mái tuyệt vời.</p>
+                <p> Với hơn 20 năm hoạt động trong lĩnh vực điện lạnh, thương hiệu điều hòa XuHao đã được
+                    khẳng định bởi hàng nghìn đại lý cả nước tin tưởng và sử dụng sản phẩm cũng như dịch vụ của công ty.
+                </p>
+                <p>Công ty cổ phần XuHao là nhà phân phối điều hòa lớn nhất miền Bắc hiện nay,
+                    chuyên cung cấp các sản phẩm điều hòa, điện lạnh, gia dụng và các giải pháp thi công các dự án công
+                    trình điện lạnh, điều hòa trung tâm và hệ thống thông gió.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
